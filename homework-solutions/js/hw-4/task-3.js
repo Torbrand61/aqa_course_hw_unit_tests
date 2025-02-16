cf33639c610ba5b853ea3c52d0024e3fb4dd11d9
Vadim <torbzar@list.ru>
2025-02-16 10:43:38 +0300

    hw-4: modify task-1, -2, -3

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -1,8 +1,18 @@
-/*
+// Преобразовать Task 2 таким образом, чтобы значение НАПРИМЕР '2' (т.е. ЛЮБАЯ строка в которой лежат ТОЛЬКО ЦИФРЫ) пропускалось, преобразовываясь в number
 
-(НЕ ОБЯЗАТЕЛЬНО)
+let minAge = 18;
+let maxAge = 60;
+let age = `18`;
+let ageNumber = Number(age);
 
-Преобразовать Task 2 таким образом, чтобы значение НАПРИМЕР '2' (т.е. ЛЮБАЯ строка в которой лежат ТОЛЬКО ЦИФРЫ) пропускалось, 
-  преобразовываясь в number
-
-*/
+if (Number.isNaN(ageNumber)) {
+  console.log (`Incorrect data type`);
+} else if (ageNumber < minAge) {
+  console.log(`You don't have access cause your age is ${ageNumber}. It's less then 18`);
+} else if (ageNumber >= minAge && ageNumber < maxAge) {
+  console.log(`Welcome!`);
+} else if (ageNumber > maxAge) {
+  console.log(`Keep calm and look Culture channel`);
+} else {
+  console.log(`Technical work`);
+}
