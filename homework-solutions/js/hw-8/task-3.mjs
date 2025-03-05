401939fd9b82ffe4a9c8ce10e69369347f15241c
vmusinov <vmusinov@tradingview.com>
2025-03-05 11:00:43 +0300

    hw-8: modify task-3

--- homework-solutions/js/hw-8/task-3.mjs
+++ homework-solutions/js/hw-8/task-3.mjs
@@ -7,11 +7,24 @@
 */
 
 function findMissingNumber(numbers) {
-  const n = numbers.length + 1;
-  const expectedSum = (n * (n + 1)) / 2;
-  const actualSum = numbers.reduce((sum, num) => sum + num, 0);
+  numbers.sort((a, b) => a - b);
 
-  return expectedSum - actualSum;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] !== i + 1) {
+      return i + 1;
+    }
+  }
+
+  return numbers.length + 1;
 }
+console.log(findMissingNumber([5,2,7,3,8,1,6]))
+
+// function findMissingNumber(numbers) {
+//   const n = numbers.length + 1;
+//   const expectedSum = (n * (n + 1)) / 2;
+//   const actualSum = numbers.reduce((sum, num) => sum + num, 0);
+
+//   return expectedSum - actualSum;
+// }
 
 export { findMissingNumber };
